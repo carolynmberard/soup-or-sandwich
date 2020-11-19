925aeae7bbe7bde7650feca49061bc2857eedc29
Carolyn Berard <carolynmberard@gmail.com>
2020-11-19 17:28:33 -0600

    Feature: app intro screen & splash

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import logo from './assets/soup-sandwich.jpeg';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} style={styles.logo} />
+      <Text style={styles.instructions}>To join the Soup or Sandwich debate, just press the button below!</Text>
+      <TouchableOpacity
+        onPress={() => alert('🥪 Jk, everything is a sandwich 🥪')}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Choose a Side</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +25,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 300,
+    height: 300,
+    marginBottom: 10,
+    borderRadius: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: '#00ABA3',
+    padding: 15,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  }
 });
